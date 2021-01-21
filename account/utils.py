9b78e5819694bfc5bb7dbb4bb9d5bfa75bd4506f
Meerim <meerim.ernesova@mail.ru>
2021-01-21 20:53:54 +0600

    change url

--- account/utils.py
+++ account/utils.py
@@ -6,7 +6,7 @@ def send_activation_email(user):
     subject = 'Thanks for registration'
     body = 'Thanks for registration in our site.\n'\
         'For account activation go for link below:\n'\
-            f'http://127.0.0.1:8000/v1/account/activate/{user.activation_code}/'
+            f'http://http://demo-meerim.herokuapp.com/v1/account/activate/{user.activation_code}/'
     from_email = 'e-shop@django.kg'
     recipients = [user.email]
     send_mail(subject=subject, message=body, from_email=from_email, recipient_list=recipients)
